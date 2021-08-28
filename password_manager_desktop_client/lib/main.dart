96b05904afd6699a9786d09f46fd351271ed44ba
Eren <eren.simsek@tutanota.com>
2021-08-28 11:53:16 +0300

    mvp

--- password_manager_desktop_client/lib/main.dart
+++ password_manager_desktop_client/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
 
 void main() {
   runApp(const MyApp());
@@ -12,34 +13,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'The Single Best Password Manager Ever'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,68 +32,183 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+  final websiteController = TextEditingController();
 
-  void _incrementCounter() {
+  String username = "";
+  String password = "";
+  String website = "";
+
+  @override
+  void initState() {
+    super.initState();
+    usernameController.addListener(_updateUsername);
+    passwordController.addListener(_updatePassword);
+    websiteController.addListener(_updateWebsite);
+  }
+
+  @override
+  void dispose() {
+    usernameController.dispose();
+    passwordController.dispose();
+    websiteController.dispose();
+    super.dispose();
+  }
+
+  _updateUsername() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      username = usernameController.text;
     });
+  }
+
+  _updatePassword() {
+    setState(() {
+      password = passwordController.text;
+    });
+  }
+
+  _updateWebsite() {
+    setState(() {
+      website = websiteController.text;
+    });
+  }
+
+  void _saveNewPassword(String username, String password, String website) {
+    ScaffoldMessenger.of(context).hideCurrentSnackBar();
+    if (username.isNotEmpty && password.isNotEmpty && website.isNotEmpty) {
+      Process.run('pwdm', ['-w', website, username, password]).then((value) =>
+          value.stderr.toString().isNotEmpty
+              ? ScaffoldMessenger.of(context)
+                  .showSnackBar(snackbar(value.stderr))
+              : null);
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+          snackbar('Supply a username, password, and domain name!'));
+    }
+  }
+
+  void _deletePassword(String username, String website) {
+    ScaffoldMessenger.of(context).hideCurrentSnackBar();
+    if (username.isNotEmpty && website.isNotEmpty) {
+      Process.run('pwdm', ['-d', website, username]).then((value) => value
+              .stderr
+              .toString()
+              .isNotEmpty
+          ? ScaffoldMessenger.of(context).showSnackBar(snackbar(value.stderr))
+          : null);
+    } else {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(snackbar('Supply a username, and domain name!'));
+    }
+  }
+
+  void _updateDbPassword(String username, String password, String website) {
+    ScaffoldMessenger.of(context).hideCurrentSnackBar();
+    if (username.isNotEmpty && password.isNotEmpty && website.isNotEmpty) {
+      Process.run('pwdm', ['-c', website, username, password]).then((value) =>
+          value.stderr.toString().isNotEmpty
+              ? ScaffoldMessenger.of(context)
+                  .showSnackBar(snackbar(value.stderr))
+              : null);
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+          snackbar('Supply a username, password, and domain name!'));
+    }
+  }
+
+  void _readPassword(String username, String website) {
+    ScaffoldMessenger.of(context).hideCurrentSnackBar();
+    if (username.isNotEmpty && website.isNotEmpty) {
+      Process.run('pwdm', ['-r', website, username]).then((value) => value
+              .stderr
+              .toString()
+              .isNotEmpty
+          ? ScaffoldMessenger.of(context)
+              .showSnackBar(snackbar("Record not found! \n" + value.stderr))
+          : ScaffoldMessenger.of(context).showSnackBar(snackbar(value.stdout)));
+    } else {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(snackbar('Supply a username, and domain name!'));
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+          children: [
+            inputField(usernameController, 'Username',
+                const Icon(Icons.account_circle)),
+            inputField(passwordController, 'Password', const Icon(Icons.lock)),
+            inputField(websiteController, 'Website', const Icon(Icons.web)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () =>
+                      _saveNewPassword(username, password, website),
+                  child: const Text('Save New Password'),
+                ),
+                ElevatedButton(
+                  onPressed: () => _deletePassword(username, website),
+                  child: const Text('Delete Password'),
+                ),
+                ElevatedButton(
+                  onPressed: () =>
+                      _updateDbPassword(username, password, website),
+                  child: const Text('Update Password'),
+                ),
+                ElevatedButton(
+                  onPressed: () => _readPassword(username, website),
+                  child: const Text('Read Password'),
+                ),
+              ],
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+Widget inputField(TextEditingController controller, String hint, Icon ikon) {
+  return Padding(
+    padding: const EdgeInsets.fromLTRB(300, 20, 300, 20),
+    child: TextField(
+      cursorColor: Colors.blue,
+      controller: controller,
+      style: const TextStyle(color: Colors.blue),
+      decoration: InputDecoration(
+        hintText: hint,
+        hintStyle: const TextStyle(color: Colors.blue),
+        icon: ikon,
+        enabledBorder: const UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.blue)),
+        focusedBorder: const OutlineInputBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(5),
+            ),
+            borderSide: BorderSide(color: Colors.white)),
+      ),
+    ),
+  );
+}
+
+SnackBar snackbar(txt) {
+  return SnackBar(
+    duration: const Duration(seconds: 10),
+    content: Row(
+      children: [
+        const Icon(Icons.error_outline),
+        const SizedBox(width: 30),
+        Flexible(child: Text(txt)),
+      ],
+    ),
+  );
 }
